04a998d7dd85ed1646e55144cf9d9eb1932220f3
Tom Bell <ctombell@gmail.com>
2024-04-19 15:32:55 +1200

    store stream names and ids in a dict
    nhltv can give multiple options for which stream to watch for any given
    game, and the names given to the streams can vary. This commit allows
    any number of streams with any stream name to be presented to the user.
    
    Resolves: #53

--- addon.py
+++ addon.py
@@ -5,12 +5,7 @@ url = None
 name = None
 mode = None
 game_day = None
-stream1_id = None
-stream2_id = None
-stream3_id = None
-stream1_name = None
-stream2_name = None
-stream3_name = None
+stream_ids = None
 highlight_id = None
 teams_stream = None
 stream_date = None
@@ -23,18 +18,8 @@ if "mode" in params:
     mode = int(params["mode"])
 if "game_day" in params:
     game_day = urllib.unquote_plus(params["game_day"])
-if "stream1_id" in params:
-    stream1_id = urllib.unquote_plus(params["stream1_id"])
-if "stream2_id" in params:
-    stream2_id = urllib.unquote_plus(params["stream2_id"])
-if "stream3_id" in params:
-    stream3_id = urllib.unquote_plus(params["stream3_id"])
-if "stream1_name" in params:
-    stream1_name = urllib.unquote_plus(params["stream1_name"])
-if "stream2_name" in params:
-    stream2_name = urllib.unquote_plus(params["stream2_name"])
-if "stream3_name" in params:
-    stream3_name = urllib.unquote_plus(params["stream3_name"])
+if "stream_ids" in params:
+    stream_ids = ast.literal_eval(params["stream_ids"])
 if "highlight_id" in params:
     highlight_id = urllib.unquote_plus(params["highlight_id"])
 
@@ -46,7 +31,7 @@ elif mode == 100 or mode == 101:
     todays_games(game_day)
 
 elif mode == 104:
-    stream_select(stream1_id, stream2_id, stream3_id, stream1_name, stream2_name, stream3_name, highlight_id)
+    stream_select(stream_ids, highlight_id)
 
 elif mode == 105:
     # Yesterday"s Games
